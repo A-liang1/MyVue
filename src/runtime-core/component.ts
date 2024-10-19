1f096d3cddda5a703ed9e90edb73595417fca576
高宏亮 <2077542643@qq.com>
2024-10-19 09:35:09 +0800

    currentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -29,11 +29,12 @@ function setupStatefulComponent(instance: any) {
 
   const { setup } = Component;
   if (setup) {
+    setCurrentInstance(instance);
     // funciton / Object
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     });
-
+    setCurrentInstance(null);
     handleSetupResult(instance, setupResult);
   }
 }
@@ -49,4 +50,11 @@ function handleSetupResult(instance, setupResult: any) {
 function finishComponentSetup(instance: any) {
   const Component = instance.type;
   instance.render = Component.render;
+}
+let currentInstance = null;
+export function getCurrentInstance() {
+  return currentInstance;
+}
+export function setCurrentInstance(instance) {
+  currentInstance = instance;
 }
