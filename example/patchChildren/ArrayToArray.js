837e090b5381536fb3ab363cdabc9c6ac1d87fe9
高宏亮 <2077542643@qq.com>
2024-10-22 20:01:26 +0800

    双端对比算法（二）中间部分的删除

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -67,7 +67,48 @@ import { ref, h } from "../../lib/guide-mini-vue.esm.js";
 //   h("p", { key: "B" }, "b"),
 //   h("p", { key: "C" }, "c"),
 // ];
-// const nextChildren = [h("p", { key: "B" }, "b"), h("p", { key: "C" }, "c")];
+// const nextChildren =  [h("p", { key: "B" }, "b"), h("p", { key: "C" }, "c")];
+// 5.删除牢大 (在牢大里面存在，新的里面不存在)
+// 5.1
+// a,b,(c,d),f,g
+// a,b,(e,c),f,g
+// const prevChildren = [
+//   h("p", { key: "A" }, "a"),
+//   h("p", { key: "B" }, "b"),
+//   h("p", { key: "C", id: "c-prev" }, "c"),
+//   h("p", { key: "D" }, "d"),
+//   h("p", { key: "F" }, "f"),
+//   h("p", { key: "G" }, "g"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "a"),
+//   h("p", { key: "B" }, "b"),
+//   h("p", { key: "E" }, "e"),
+//   h("p", { key: "C", id: "c-next" }, "c"),
+//   h("p", { key: "F" }, "f"),
+//   h("p", { key: "G" }, "g"),
+// ];
+// 5.1.1
+// a,b,(c,e,d),f,g
+// a,b,(e,c),f,g
+// 中间部分，牢大比新的多，那么多出来的直接就可以被干掉(优化删除逻辑)
+const prevChildren = [
+  h("p", { key: "A" }, "a"),
+  h("p", { key: "B" }, "b"),
+  h("p", { key: "C", id: "c-prev" }, "c"),
+  h("p", { key: "E" }, "e"),
+  h("p", { key: "D" }, "d"),
+  h("p", { key: "F" }, "f"),
+  h("p", { key: "G" }, "g"),
+];
+const nextChildren = [
+  h("p", { key: "A" }, "a"),
+  h("p", { key: "B" }, "b"),
+  h("p", { key: "E" }, "e"),
+  h("p", { key: "C", id: "c-next" }, "c"),
+  h("p", { key: "F" }, "f"),
+  h("p", { key: "G" }, "g"),
+];
 export default {
   name: "ArrayToArray",
   setup() {
